868015aab5535649ab4d4afd7b6aff8f1a1f2f9d
Junwoo Seo <junwoo.seo.1998@gmail.com>
2024-08-20 11:52:21 -0700

    format document

--- main.py
+++ main.py
@@ -3,56 +3,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import math
-def kmeanCompute(data : np.array, ks : np.array):
+
+
+def kmeanCompute(data: np.array, ks: np.array):
     # time0 = time.time()
     cluster = [0 for i in range(data.shape[0])]
-    '''
+    """
     #slow
     dist_list = [0 for i in range(ks.shape[0])]
     for i in range(data.shape[0]):
         for k in range(ks.shape[0]):
             dist_list[k]=np.sum(np.square(data[i] - ks[k]))
         cluster[i]=np.argmin(dist_list)
-    '''
+    """
     labels = []
     for k in range(ks.shape[0]):
         # print(np.sum(np.square((data - ks[k])), axis=1).shape)
-        labels.append(np.sum(np.square(data-ks[k]), axis = 1))
+        labels.append(np.sum(np.square(data - ks[k]), axis=1))
     for i in range(data.shape[0]):
         min_k = -1
         min_val = math.inf
         for k in range(ks.shape[0]):
-            if(min_val>labels[k][i]):
+            if min_val > labels[k][i]:
                 min_k = k
-                min_val=labels[k][i]
+                min_val = labels[k][i]
         cluster[i] = min_k
     # print("cluster time took: ", time.time() - time0)
     return cluster
 
-def recomputeMeans(data : np.array, cluster : list, ks : np.array):
+
+def recomputeMeans(data: np.array, cluster: list, ks: np.array):
     result = np.zeros(shape=ks.shape)
     # time0 = time.time()
     for i in range(ks.shape[0]):
-        result[i] = np.average(data[np.array(cluster) == i], axis = 0)
+        result[i] = np.average(data[np.array(cluster) == i], axis=0)
     # print("recomputeMeans time took: ", time.time() - time0)
     return result
-def kmean(data : np.array, ks : np.array):
+
+
+def kmean(data: np.array, ks: np.array):
     cluster = kmeanCompute(data, ks)
     # print("cluster: ",cluster)
     ks = recomputeMeans(data, cluster, ks)
     # print("recomputed centroids: ", ks)
     prev = cluster
-    while(True):
+    while True:
         cluster = kmeanCompute(data, ks)
         # print("cluster: ", cluster)
-        if(cluster==prev):
-            #print("final centroids: ", ks)
+        if cluster == prev:
+            # print("final centroids: ", ks)
             break
         ks = recomputeMeans(data, cluster, ks)
         # print("recomputed centroids: ", ks)
         prev = cluster
 
     return ks
+
 
 def GenImage(image_file, k, prefix):
     ks = np.array([[i, i, i] for i in range(0, 256, 255 // (k - 1))])
@@ -74,34 +80,40 @@ def GenImage(image_file, k, prefix):
     for i in range(flat_data.shape[0]):
         flat_data[i] = centorid[cluster_labels[i]]
 
-    cv2.imwrite('{}_k_{}.jpg'.format(prefix, str(k)), cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(
+        "{}_k_{}.jpg".format(prefix, str(k)), cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+    )
+
 
 def GenImages(filename, maxK, prefix):
-    for i in range(3, maxK+1):
+    for i in range(3, maxK + 1):
         GenImage(filename, i, prefix)
 
+
 def ShowImg(fileName):
-    plt.rcParams['figure.figsize']=[8,6]
+    plt.rcParams["figure.figsize"] = [8, 6]
     fig = plt.figure()  # rows*cols
     rows = 3
     cols = 3
     i = 1
-    for index in range(rows*cols):
+    for index in range(rows * cols):
         ax = fig.add_subplot(rows, cols, i)
-        img=None
-        if(i==1):
-            img = cv2.imread('{}.jpg'.format(fileName))
+        img = None
+        if i == 1:
+            img = cv2.imread("{}.jpg".format(fileName))
             ax.set_xlabel("original")
         else:
-            print('{}_k_{}.jpg'.format(fileName, i+1))
-            img = cv2.imread('{}_k_{}.jpg'.format(fileName, i+1))
-            ax.set_xlabel("k = {}".format(i+1))
+            print("{}_k_{}.jpg".format(fileName, i + 1))
+            img = cv2.imread("{}_k_{}.jpg".format(fileName, i + 1))
+            ax.set_xlabel("k = {}".format(i + 1))
 
         ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
         ax.set_xticks([]), ax.set_yticks([])
         i += 1
     plt.show()
+
+
 def main():
 
     print("img1 result: ")
@@ -116,12 +128,11 @@ def main():
     GenImages("img3.jpg", 10, "img3")
     print()
 
-
     ShowImg("img1")
     ShowImg("img2")
     ShowImg("img3")
 
-    '''
+    """
     #just to check
     k=10
     ks=np.array([[i,i,i] for i in range(0, 256, 255//(k-1))])
@@ -151,8 +162,8 @@ def main():
     ax[1].imshow(new_image)
     cv2.imwrite('k_{}.jpg'.format(str(k)), cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR))
     plt.show()
-    '''
+    """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
